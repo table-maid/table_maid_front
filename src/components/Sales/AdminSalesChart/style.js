efebd03ee6697c3ba011223adb0f2f9aea8c4021
ez0603 <dlwldjs3132@naver.com>
2024-07-01 10:52:51 +0900

    Fix : 매출관리 CSS 수정

--- src/components/Sales/AdminSalesChart/style.js
+++ src/components/Sales/AdminSalesChart/style.js
@@ -2,7 +2,7 @@ import { css } from "@emotion/react";
 
 export const dataLayout = css`
 	width: 100%;
-	height: 170px;
+	height: 270px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
