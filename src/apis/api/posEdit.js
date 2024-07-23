7fe12b1d021852ec334565660dfb0063f5f68ead
leesongchi <kyoti0414@naver.com>
2024-07-23 13:15:19 +0900

    Feat: 포스설정 조회

--- src/apis/api/posEdit.js
+++ src/apis/api/posEdit.js
@@ -2,4 +2,8 @@ import instance from "../utils/instance"
 
 export const saveFloorTableRequest = async (data) => {
     return await instance.post("/pos/floor/table", data)
-}
+};
+
+export const selectFloorTableRequest = async (adminId) => {
+    return await instance.get("/pos/floor/table", { params: { adminId } });
+};
